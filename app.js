53249127ba54f2a7d0ba46e017e7629aede4846f
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-08-19 13:35:50 +0600

    changing sounds

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
     this.playBtn = document.querySelector(".play");
+    this.currentKick = "sounds/kick-classic.wav";
+    this.currentSnare = "sounds/snare-acoustic01.wav";
+    this.currentHihat = "sounds/hihat-acoustic01.wav";
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 150;
     this.isPlaying = null;
+    this.selects = document.querySelectorAll("select");
   }
   activePad() {
     console.log(this.classList.toggle("active"));
@@ -49,6 +53,7 @@ class DrumKit {
       }, interval);
     }
   }
+
   updateButton() {
     if (!this.isPlaying) {
       this.playBtn.innerHTML = "Stop";
@@ -58,11 +63,29 @@ class DrumKit {
       this.playBtn.classList.remove("active");
     }
   }
+
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+
+    switch (selectionName) {
+      case "kick-select":
+        this.kickAudio.src = selectionValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = selectionValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = selectionValue;
+        break;
+    }
+  }
 }
 
 const drumKit = new DrumKit();
 // drumKit.start();
 
+// add event listener
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
   pad.addEventListener("animationend", function () {
@@ -73,4 +96,10 @@ drumKit.pads.forEach((pad) => {
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.updateButton();
   drumKit.start();
+});
+
+drumKit.selects.forEach((select) => {
+  select.addEventListener("change", function (e) {
+    drumKit.changeSound(e);
+  });
 });
